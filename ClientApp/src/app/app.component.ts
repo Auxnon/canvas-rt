f9f3dd17465db3a38d5b2d1df2235856fc943ad1
Makeavoy <auxnon@gmail.com>
2023-11-28 01:08:07 -0500

    feat: standalone comps + migrations per ang17

--- ClientApp/src/app/app.component.ts
+++ ClientApp/src/app/app.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
+import { RouterOutlet } from "@angular/router";
+import { NgSwitch, NgSwitchDefault, NgSwitchCase } from "@angular/common";
+import { ArtViewComponent } from "./art-view/art-view.component";
 
 @Component({
-    selector: 'app-root',
-    templateUrl: './app.component.html',
-    styleUrls: ['./app.component.scss']
+    selector: "app-root",
+    templateUrl: "./app.component.html",
+    styleUrls: ["./app.component.scss"],
+    standalone: true,
+    imports: [
+        ArtViewComponent,
+        NgSwitch,
+        NgSwitchDefault,
+        NgSwitchCase,
+        RouterOutlet,
+    ],
 })
 export class AppComponent {
-    title = 'canvas_rt2';
+    title = "canvas_rt2";
 }
