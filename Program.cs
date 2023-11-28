9cafa02adc2ec1651fa0cefc576e8840fc301827
Makeavoy <auxnon@gmail.com>
2023-11-28 01:08:32 -0500

    feat: wip, websocket c#

--- Program.cs
+++ Program.cs
@@ -15,13 +15,15 @@ if (!app.Environment.IsDevelopment())
 app.UseHttpsRedirection();
 app.UseStaticFiles();
 app.UseRouting();
+app.UseWebSockets();
 
 app.UseEndpoints(endpoints =>
 {
     endpoints.MapControllers();
 });
 
-app.UseSpa(spa => {
+app.UseSpa(spa =>
+{
     // To learn more about options for serving an Angular SPA from ASP.NET Core,
     // see https://go.microsoft.com/fwlink/?linkid=864501
     spa.Options.SourcePath = "ClientApp";
